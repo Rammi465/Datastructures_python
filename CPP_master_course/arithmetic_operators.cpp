431ec40564ce7e805994f5c128520cee3fa11755
Rammi465 <ramarao.465@gmail.com>
2025-02-28 05:43:47 -0600

    cpp course for practice

---
+++ CPP_master_course/arithmetic_operators.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+int main()
+{
+    int a = 130, b = 10;
+    int sum  = a + b;
+    int difference = a - b;
+    int product = a * b;
+    int quotient = a / b ;
+    int reminder = a % b;
+    int result = 10;
+    int myInt =  15;
+
+
+
+    result += 15;
+
+    std::cout << "Sum = " << sum << std::endl;
+    std::cout << "Difference = " << difference << std::endl;
+    std::cout << "product = " <<  product << std::endl;
+    std::cout << "quotient = " << quotient << std::endl;
+    std::cout << "reminder = " << reminder << std::endl;
+    std::cout << "result = " << result << std::endl;
+
+    std::cout << "myInt = " << myInt << std::endl;
+    myInt++;
+    std::cout << myInt << std::endl;
+    
+
+   return 0;
+
+}
